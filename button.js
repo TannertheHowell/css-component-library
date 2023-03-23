104d8e601e07d9a0425439d3192382f3c1a76624
Tanner Howell <a02079061@usu.edu>
2023-03-23 16:53:42 -0600

    Found and cut useless opacity section, cleaned up code block indenting

--- button.js
+++ button.js
@@ -2,10 +2,10 @@ const fancyButton = document.querySelector('.fancy-button');
 const icon = document.querySelector('.material-icons.md-36');
 
 fancyButton.addEventListener('click', () => {
-  fancyButton.classList.add('saving');
-  icon.textContent = 'Saving...';
-  setTimeout(() => {
-    fancyButton.classList.remove('saving');
-    icon.textContent = 'save';
-  }, 2100);
+    fancyButton.classList.add('saving');
+    icon.textContent = 'Saving...';
+    setTimeout(() => {
+        fancyButton.classList.remove('saving');
+        icon.textContent = 'save';
+    }, 2100);
 });
