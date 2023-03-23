340afd1e57b19394a065317ce8180f861bb44606
Tanner Howell <a02079061@usu.edu>
2023-03-23 17:45:20 -0600

    Caught a bug where clicking the FAB triggered an animation on the other button with an icon. Added specificity to the javascript file

--- button.js
+++ button.js
@@ -1,5 +1,6 @@
 const floatingButton = document.querySelector('.floating-button');
-const icon = document.querySelector('.material-icons.md-36');
+const icon = document.querySelector('.floating-button .material-icons.md-36');
+
 
 floatingButton.addEventListener('click', () => {
     floatingButton.classList.add('saving');
