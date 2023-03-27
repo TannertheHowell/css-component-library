c3ed2ca3ff15d999ceae2682c680430c31efe274
Tanner Howell <a02079061@usu.edu>
2023-03-27 17:09:11 -0600

    Got the image wheel finally loading in right, animating between the images and going automatically

--- imageCar.js
+++ imageCar.js
@@ -1,8 +1,19 @@
-const images = [
-    "LavaSnail.png",
-    "Better Lava Snail.jpg",
-    "snailRacing.jpg",
-    "snailSkydiving.jpg",
-    "snailSurfing.jpg",
-  ];
-  
+const carousel = document.querySelector('.carousel-container');
+const images = carousel.querySelectorAll('img');
+const imageCount = images.length;
+let counter = 1;
+
+setInterval(() => {
+  carousel.style.transform = `translateX(${-400 * counter}px)`;
+  counter++;
+  if (counter >= imageCount) {
+    counter = 0;
+    setTimeout(() => {
+      carousel.style.transition = 'none';
+      carousel.style.transform = 'translateX(0)';
+      setTimeout(() => {
+        carousel.style.transition = 'transform 2s ease-in-out';
+      }, 50);
+    }, 8000);
+  }
+}, 7000);
